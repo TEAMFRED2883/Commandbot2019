3f5fd51af42772bface47ac7a3d9650fa8faa091
TEAMFRED2883 <jeremy_culleton@warroad.k12.mn.us>
2018-12-20 13:28:22 -0600

    Added Drivetrain Code

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -8,6 +8,7 @@ package frc.robot;
 import frc.robot.RobotMap;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 
@@ -22,6 +23,7 @@ public class OI {
   // You create one by telling it which joystick it's on and which button
   // number it is.
    Joystick stick = new Joystick(RobotMap.JoystickPort);
+   XboxController xboxCon = new XboxController(RobotMap.JoystickPort);
    Button button = new JoystickButton(stick, RobotMap.A_button);
 
   // There are a few additional built in buttons you can use. Additionally,
@@ -43,4 +45,10 @@ public class OI {
   // Start the command when the button is released and let it run the command
   // until it is finished as determined by it's isFinished method.
   // button.whenReleased(new ExampleCommand());
+
+  /**Returns the Our from the oi class Xboxcontroller */
+  public XboxController getConch()
+  {
+      return xboxCon;
+  }
 }
